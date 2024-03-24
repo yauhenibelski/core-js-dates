53dac4b5e3c529f611a05a7e0e0951c865547e7b
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-03-24 20:47:20 +0100

    feat: getWorkSchedule

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -266,8 +266,32 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const start = new Date(period.start.split('-').reverse().join('-'));
+  const end = new Date(period.end.split('-').reverse().join('-'));
+  let cwd = 0;
+  let cfd = countOffDays;
+  const res = [];
+
+  while (Date.parse(start.toString()) <= Date.parse(end.toString())) {
+    if (cwd < countWorkDays) {
+      cwd += 1;
+      cfd = countOffDays;
+      res.push(
+        `${`${start.getDate()}`.padStart(2, '0')}-${`${start.getMonth() + 1}`.padStart(2, '0')}-${start.getFullYear()}`
+      );
+    }
+    if (!cfd) {
+      cwd = 0;
+    }
+    if (cwd === countWorkDays) {
+      cfd -= 1;
+    }
+
+    start.setDate(start.getDate() + 1);
+  }
+
+  return res;
 }
 
 /**
