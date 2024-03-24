051c958d5d811f4ca6f52053ee7f7ef82dc39082
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-03-24 16:58:58 +0100

    feat: getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -216,8 +216,18 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  let day = null;
+
+  while (!day) {
+    date.setDate(date.getDate() + 1);
+
+    if (date.getDay() === 5 && date.getDate() === 13) {
+      day = date;
+    }
+  }
+
+  return day;
 }
 
 /**
